9a75147f243ca5a311bdb2682754405d225ec66e
Chiyu Zhong <zhongchiyu@gmail.com>
2018-02-07 16:29:37 +0800

    add genstage simple example

---
+++ lib/producer_consumer.ex
@@ -0,0 +1,58 @@
+defmodule IntroGenStage.Producer do
+  use GenStage
+
+  def start_link(initial \\ 0) do
+    GenStage.start_link(__MODULE__, initial, name: __MODULE__)
+  end
+
+  def init(counter), do: {:producer, counter}
+
+  def handle_demand(demand, state) do
+    events = Enum.to_list(state..(state + demand - 1))
+    {:noreply, events, state + demand}
+  end
+end
+
+defmodule IntroGenStage.ProducerConsumer do
+  use GenStage
+
+  require Integer
+
+  def start_link do
+    GenStage.start_link(__MODULE__, :state_doesnt_matter, name: __MODULE__)
+  end
+
+  def init(state) do
+    {:producer_consumer, state, subscribe_to: [IntroGenStage.Producer]}
+  end
+
+  def handle_events(events, _from, state) do
+    numbers =
+      events
+      |> Enum.filter(&Integer.is_even/1)
+
+    {:noreply, numbers, state}
+  end
+end
+
+defmodule IntroGenStage.Consumer do
+  use GenStage
+
+  def start_link do
+    GenStage.start_link(__MODULE__, :state_doesnt_matter)
+  end
+
+  def init(state) do
+    {:consumer, state, subscribe_to: [IntroGenStage.ProducerConsumer]}
+  end
+
+  def handle_events(events, _from, state) do
+    for event <- events do
+      :timer.sleep(1000)
+      IO.inspect({self(), event, state})
+    end
+
+    # As a consumer we never emit events
+    {:noreply, [], state}
+  end
+end
