9b78570386d40393f46f0a892b0dcda5782fe73b
Chiyu Zhong <1606069+CatTail@users.noreply.github.com>
2018-02-07 16:29:37 +0800

    add genstage simple example

--- test.ex
+++ test.ex
@@ -7,3 +7,10 @@ alias IntroGenStage.StateServer
 {_, state_server} = StateServer.start_link()
 event = %{device_id: "xxx-xxx-xxx", ctx: "123", value: "50.5"}
 StateServer.process(state_server, event)
+
+alias IntroGenStage.Producer
+alias IntroGenStage.ProducerConsumer
+alias IntroGenStage.Consumer
+{_, source} = Producer.start_link()
+{_, flow} = ProducerConsumer.start_link()
+{_, sink} = Consumer.start_link()
